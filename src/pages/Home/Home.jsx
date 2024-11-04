10961fe9cdf26b52db1e462741df62682d4edd9e
Lyb1ca <vicenteillanesayala@gmail.com>
2024-11-04 20:56:24 -0300

    Home changed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className="flex flex-row justify-between px-28 m-auto space-x-32">
-            <Link to="talleres">Talleres</Link>
-            <Link to="selecciones">Selecciones</Link>
-            <Link to="gimnasio">Gimnasio</Link>
-            <Link to="canchas">Canchas</Link>
+        <div className="flex flex-row justify-between px-28 m-auto space-x-32 text-3xl">
+            <Link className="btn-card" to="talleres">Talleres</Link>
+            <Link className="btn-card" to="selecciones">Selecciones</Link>
+            <Link className="btn-card" to="gimnasio">Gimnasio</Link>
+            <Link className="btn-card" to="canchas">Canchas</Link>
         </div>
     )
 }
