d4ca95651f86afdc45475e81b476f05316cb9bcd
El-Vitas <jonathan.olivares.salinas@gmail.com>
2024-11-13 18:05:43 -0300

    Style: Change 'Inscrito' button color in 'talleres'

--- src/components/CourseBigCard/CourseBigCard.jsx
+++ src/components/CourseBigCard/CourseBigCard.jsx
@@ -25,7 +25,7 @@ const CourseBigCard = ({ props }) => {
                     <div className='mt-8'>
                         <button 
                             // className='hover:bg-sky-300 border rounded-xl px-6 py-2'
-                            className='btn-card'
+                            className={enrolled ? 'btn-disabled' : 'btn-card'} 
                             onClick={handleEnroll}
                             disabled={enrolled} 
                         >
