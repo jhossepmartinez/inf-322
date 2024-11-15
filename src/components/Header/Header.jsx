c5b5ec182b08abb0e6b365165c118406a9b080b5
jhossepmartinez <jhossepmv@gmail.com>
2024-11-14 22:08:35 -0300

    make header options bigger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <div className="Header flex flex-row justify-between">
-            <Link className="btn-header font-medium" to="talleres">Talleres</Link>
-            <Link className="btn-header font-medium" to="selecciones">Selecciones</Link>
-            <Link className="btn-header font-medium" to="gimnasio">Gimnasios</Link>
-            <Link className="btn-header font-medium" to="canchas">Canchas</Link>
+            <Link className="btn-header font-medium text-xl" to="talleres">Talleres</Link>
+            <Link className="btn-header font-medium text-xl" to="selecciones">Selecciones</Link>
+            <Link className="btn-header font-medium text-xl" to="gimnasio">Gimnasios</Link>
+            <Link className="btn-header font-medium text-xl" to="canchas">Canchas</Link>
         </div>
     )
 }
