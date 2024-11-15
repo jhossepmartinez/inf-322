80ecab78c18144da9089827800ef98fb6f72e9f3
jhossepmartinez <jhossepmv@gmail.com>
2024-11-14 22:08:52 -0300

    make options the first in the page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div className='flex-row items-center justify-center space-y-32 mt-12'>
+            <div className="flex flex-row justify-between px-28 m-auto space-x-32 text-3xl">
+                <Link className="btn-card" to="talleres">Talleres</Link>
+                <Link className="btn-card" to="selecciones">Selecciones</Link>
+                <Link className="btn-card" to="gimnasio">Gimnasio</Link>
+                <Link className="btn-card" to="canchas">Canchas</Link>
+            </div>
             <div className="flex items-center justify-center">
                 <div className="flex flex-col items-center space-y-32">
                     <div className="w-full max-w-6xl space-y-12"> 
@@ -12,12 +18,7 @@ const Home = () => {
                 </div>
             </div>
             
-            <div className="flex flex-row justify-between px-28 m-auto space-x-32 text-3xl">
-                <Link className="btn-card" to="talleres">Talleres</Link>
-                <Link className="btn-card" to="selecciones">Selecciones</Link>
-                <Link className="btn-card" to="gimnasio">Gimnasio</Link>
-                <Link className="btn-card" to="canchas">Canchas</Link>
-            </div>
+
         </div>
     )
 }
